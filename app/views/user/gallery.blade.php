9c7470df0e33940d5adb28373059fff573437b56
mctr <mesut.cittir@bil.omu.edu.tr>
2014-03-16 13:51:15 +0200

    file upload done

--- app/views/user/gallery.blade.php
+++ app/views/user/gallery.blade.php
@@ -40,7 +40,7 @@
 					<div class="box-content">
 
 						<div class="control-group">
-							  <label class="control-label" for="date01">Fotograf</label>
+							  <label class="control-label" for="date01">Fotograf :</label><br>
 							  <div class="controls">
 									{{ Form::open(['url' => 'user/file_uploads', 'files' => true, 'method' => 'post']) }}	
 						        	{{ Form::file('image', array('class' => 'input-file uniform_on')) }}<br><br>
@@ -72,9 +72,10 @@
 						<ul class="thumbnails gallery">
 							@foreach($images as $image)
 							<li id="image-1" class="thumbnail">
-								<a style="background:url({{ $image->image_path }})" title="Sample Image 1" href="{{ $image->image_path }}"><img class="grayscale" src="{{ $image->image_path }}" alt="Sample Image 1"></a>
+								<a style="background:url({{ URL::asset($image->image_path )}})" title="Sample Image 1" href="{{ URL::asset($image->image_path )}}"><img class="grayscale" src="{{ URL::asset($image->image_path )}}" alt="Sample Image 1"></a>
 							</li>
 							@endforeach
+							<!--<img src="{{ URL::asset('uploads/images/trt.jpg')}}" />-->
 						</ul>
 					</div>
 				</div><!--/span-->
